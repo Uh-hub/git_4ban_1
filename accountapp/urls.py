a1417217b5af0e739a9b9a5e5294ef2b83b5e743
noa_on_air <tabacal3590@gmail.com>
2021-07-02 11:33:30 +0900

    7/2 gwang in sa

---
+++ accountapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from accountapp.views import hello_world
+
+
+
+urlpatterns = [  # 어디로 갈지? 그 주소로 가도록 연결 시켜주기
+    path('hello_world/', hello_world, name='hello_world'),
+]
