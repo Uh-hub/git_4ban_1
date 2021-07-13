e22025b22ef43c538ed8d78bf41a7483a68aec1f
noa_on_air <tabacal3590@gmail.com>
2021-07-13 12:44:52 +0900

    7/13 gwang in sa 3

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [  # 어디로 갈지? 그 주소로 가도록 연결 시켜주기
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateView.as_view(), name = 'create')
 ]
