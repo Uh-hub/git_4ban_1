a85d23c9dac81dc34f0881a2557de221e079373c
noa_on_air <tabacal3590@gmail.com>
2021-08-20 09:30:58 +0900

    8/17 gwang in sa 1

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView
+from projectapp.views import ProjectCreateView, ProjectDetailView
 
 app_name = 'projectapp'
 
@@ -8,5 +8,5 @@ app_name = 'projectapp'
 
 urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='create'),
-
+    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
 ]
